8c4a1e3991ebbaea3dda8a2cce48dabb7035d5e6
Ivan Zhao <752251080@qq.com>
2014-08-09 22:18:57 +0800

    add bb/index.html

--- urls.php
+++ urls.php
@@ -2,6 +2,7 @@
 $urls = array(
         '#^/$#' => 'index',
 		'#^/sb$#'=>'sb/index',
+		'#^/bb$#'=>'bb/index',
         '#^/user/inform$#' => 'user/inform',
 		'#^/user/firstlogin$#' => 'user/firstlogin',
         '#^/register$#' => 'register',
